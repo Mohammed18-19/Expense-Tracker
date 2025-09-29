ed5dc943af85f20046a4bb39c40fe246a3d6a9af
Mohammed18-19 <aintomar.mohamed19@gmail.com>
2025-09-29 15:19:48 +0100

    Refactor core files (init, models, routes, run) and update index template

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+from datetime import datetime
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(120), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, default=lambda:datetime.today, nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, request, make_response, flash, redirect
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/add", methods=['POST'])
+def add():
+
+    description = request.form.get('description', "").strip()
+    amount_str = request.form.get('amount', "").strip()
+    category = request.form.get('category', "").strip()
+    date_str = request.form.get('date', "").strip()
+
+    if not description or not amount_str or not category:
+        flash("Please fill description, amount, and category", "error")
+        return redirect(url_for('index'))
+
+    try: # Try the code and handle errors if any
+        amount = float(amount_str)
+        if amount <= 0:
+            raise ValueError
+    except ValueError: # raise the Error if it's
+        flash('Amount must be a positive number', 'error')
+        return redirect(url_for('index'))
+
+    print("Form received:", dict(request.form))
+    return make_response("Form received check the console")
